95a393bcbbb1187e7cb07422703384a1e90128fd
Ates Goral <ates.g@myplanet.com>
2017-01-20 01:35:18 -0500

    Removed unzipping

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const crypto = require('crypto');
 
 const rp = require('request-promise-native');
 const cheerio = require('cheerio');
-const yauzl = require('yauzl');
 const mkdirp = require('mkdirp');
 const chalk = require('chalk');
 const humanizeDuration = require('humanize-duration');
@@ -17,78 +16,27 @@ const config = {
   downloadUrl: process.env.SCRAPER_DOWNLOAD_URL || 'http://gis.epa.ie/getdata/downloaddata',
   mailbackMailbox: process.env.SCRAPER_MAILBACK_MAILBOX,
   maxDocs: parseInt(process.env.SCRAPER_MAX_DOCS, 10) || Infinity,
-  startIdx: parseInt(process.env.SCRAPER_START_IDX, 10) || 0,
+  startIdx: parseInt(process.env.SCRAPER_START_IDX, 10) || 0
 };
 
 const archiveDirectory = path.join(__dirname, 'archive');
 const startedAt = new Date();
 
-function saveZipFile(zipData, name) {
+function saveFile(data, name) {
   return new Promise((resolve, reject) => {
     mkdirp(archiveDirectory, error => {
       if (error) {
         reject(error);
       } else {
-        fs.writeFile(path.join(archiveDirectory, name + '.zip'), zipData, (error) => {
+        fs.writeFile(path.join(archiveDirectory, name + '.zip'), data, (error) => {
           if (error) {
             reject(error);
           } else {
-            resolve(zipData);
+            resolve();
           }
         });
       }
     });
-  });
-}
-
-function openZipFile(zipData) {
-  return new Promise((resolve, reject) => {
-    yauzl.fromBuffer(Buffer.from(zipData), { lazyEntries: true }, (err, zipFile) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(zipFile);
-      }
-    });
-  });
-}
-
-function unpackZipFile(zipFile, destinationDirectory) {
-  return new Promise((resolve, reject) => {
-    zipFile.on('entry', entry => {
-      const fullPath = path.join(destinationDirectory, entry.fileName);
-
-      if (/\/$/.test(entry.fileName)) {
-        mkdirp(fullPath, error => {
-          if (error) {
-            reject(error);
-          } else {
-            zipFile.readEntry();
-          }
-        });
-      } else {
-        zipFile.openReadStream(entry, (error, readStream) => {
-          if (error) {
-            reject(error);
-          } else {
-            mkdirp(path.dirname(fullPath), error => {
-              if (error) {
-                reject(error);
-              } else {
-                readStream.pipe(fs.createWriteStream(fullPath));
-                readStream.on('end', () => {
-                  zipFile.readEntry();
-                });
-              }
-            });
-          }
-        });
-      }
-    });
-
-    zipFile.on('end', resolve);
-
-    zipFile.readEntry();
   });
 }
 
@@ -145,12 +93,13 @@ function archiveFile(file) {
     .then(response => {
       console.log(chalk.gray('File received'));
 
-      const zipData = response.body;
+      const data = response.body;
       const shasum = crypto.createHash('sha1');
 
-      shasum.update(zipData);
+      shasum.update(data);
 
-      // @todo check lastModifiedAt && sha1sum and skip if file already is in saved manifest
+      // @todo make if-modified-since request
+      // and check lastModifiedAt && sha1sum and skip if file already is in saved manifest
       // this would make the tool reentrant
 
       file.lastModifiedAt = new Date(response.headers['last-modified']);
@@ -160,22 +109,11 @@ function archiveFile(file) {
       console.log(chalk.gray('Last modified:', file.lastModifiedAt.toLocaleString()));
       console.log(chalk.gray('SHA-1 sum:', file.sha1sum));
 
-      return saveZipFile(zipData, file.name);
+      return saveFile(data, file.name);
     })
-    .then(zipData => {
+    .then(() => {
       console.log(chalk.gray('File saved'));
-
-      //return openZipFile(zipData);
     })
-    // @todo disabled unpacking for now -- yauzl is bailing on file ID 11
-    // .then(zipFile => {
-    //   console.log(chalk.gray('Unpacking file'));
-    //
-    //   return unpackZipFile(zipFile, path.join(archiveDirectory, file.name));
-    // })
-    // .then(() => {
-    //   console.log(chalk.gray('Unpacking done'));
-    // })
     .catch(error => {
       console.error(chalk.red('Error archiving file:', error));
     })
