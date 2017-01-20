9d674d0dfe19877f403b93c9158ad4555f8aab96
Ates Goral <ates.g@myplanet.com>
2017-01-20 09:39:28 -0500

    Added opton to skip downloading and made the tool reentrant

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ Then set the SCRAPER_MAILBACK_MAILBOX environment variable to the mailbox ID fro
 
 `SCRAPER_MAILBACK_MAILBOX=##### node index`
 
+If the scraping or downloading stalls, hit CTRL+C to quit and then restart the scraping. The tool is reentrant: it will check the existing manifest and continue scraping from where it has left off.
+
 ## Environment Variables
 
 * SCRAPER_MAILBACK_MAILBOX - **Required.** Mailback mailbox ID.
@@ -21,6 +23,7 @@ Then set the SCRAPER_MAILBACK_MAILBOX environment variable to the mailbox ID fro
 * SCRAPER_DOWNLOAD_URL - The download request submit URL. Overwrite for testing. Defaults to: http://gis.epa.ie/getdata/downloaddata
 * SCRAPER_MAX_DOCS - The maximum number of documents to request. Overwrite for testing. Defaults to nothing (i.e. request everything).
 * SCRAPER_START_IDX - The starting index (zero-based). Overwrite for testing or retrying a particular file. Defaults to 0 (i.e. start from the beginning).
+* SCRAPER_SKIP_DOWNLOAD - To skip downloading of the files and just scrape the download URLs. Set to any non-empty value (like "1"). Defaults to nothing (i.e. download ZIP files).
 
 # Findings / Approach
 
@@ -36,6 +39,6 @@ The "X-Requested-With" parameter (not HTTP header) somehow needed to be submitte
 2. Submits the download request form for each document ID while using a Mailback mailbox email address
 3. Fetches the HTML content of the email from Mailback
 4. Extracts the first link in the HTML (it points to a ZIP file)
-5. Downloads the ZIP file
-6. Saves the ZIP file under the archive folder
+5. Downloads the ZIP file (can be opted out)
+6. Saves the ZIP file under the archive folder (can be opted out)
 7. Updates manifest.json file under the archive folder
--- index.js
+++ index.js
@@ -16,35 +16,110 @@ const config = {
   downloadUrl: process.env.SCRAPER_DOWNLOAD_URL || 'http://gis.epa.ie/getdata/downloaddata',
   mailbackMailbox: process.env.SCRAPER_MAILBACK_MAILBOX,
   maxDocs: parseInt(process.env.SCRAPER_MAX_DOCS, 10) || Infinity,
-  startIdx: parseInt(process.env.SCRAPER_START_IDX, 10) || 0
+  startIdx: parseInt(process.env.SCRAPER_START_IDX, 10) || 0,
+  skipDownload: !!process.env.SCRAPER_SKIP_DOWNLOAD
 };
 
 const archiveDirectory = path.join(__dirname, 'archive');
 const startedAt = new Date();
 
-function saveFile(data, name) {
+function ensureArchiveDirectoryExists() {
   return new Promise((resolve, reject) => {
     mkdirp(archiveDirectory, error => {
       if (error) {
         reject(error);
       } else {
-        fs.writeFile(path.join(archiveDirectory, name + '.zip'), data, (error) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve();
-          }
-        });
+        resolve();
       }
     });
   });
 }
 
-function archiveFile(file) {
+function fetchFileList() {
   return Promise.resolve()
     .then(() => {
-      console.log(`Archiving file ${file.id} (${file.category.title}/${file.title})`);
+      console.log('Fetching index HTML');
 
+      return rp(config.indexUrl);
+    })
+    .then(html => {
+      console.log('Extracting file metadata');
+
+      const $ = cheerio.load(html);
+
+      const categoryList = $('.categories h3')
+        .map((idx, el) => {
+          const $el = $(el);
+
+          return {
+            id: $el.attr('class').replace(/\D/g, ''),
+            title: $el.text()
+          };
+        })
+        .get();
+
+      const categoryMap = {};
+
+      categoryList.forEach(category => {
+        categoryMap[category.id] = category;
+      });
+
+      return $('.SelectedFile')
+        .map((idx, el) => {
+          const $el = $(el);
+
+          const categoryId = $el.closest('p').attr('class').replace(/\D/g, '');
+
+          return {
+            id: $el.val(),
+            category: categoryMap[categoryId],
+            title: $el.parent().text()
+          };
+        })
+        .get();
+    });
+}
+
+function downloadFile(file) {
+  return rp({
+    uri: file.url,
+    resolveWithFullResponse: true,
+    encoding: null
+  })
+  .then(response => {
+    console.log(chalk.gray('File received'));
+
+    const data = response.body;
+    const shasum = crypto.createHash('sha1');
+
+    shasum.update(data);
+
+    // @todo make if-modified-since request
+    // and check lastModifiedAt && sha1sum and skip if file already is in saved manifest
+    // this would make the tool reentrant
+
+    file.lastModifiedAt = new Date(response.headers['last-modified']);
+    file.receivedAt = new Date();
+    file.sha1sum = shasum.digest('hex');
+
+    console.log(chalk.gray('Last modified:', file.lastModifiedAt.toLocaleString()));
+    console.log(chalk.gray('SHA-1 sum:', file.sha1sum));
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(path.join(archiveDirectory, file.name + '.zip'), data, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
+}
+
+function scrapeFileUrl(file) {
+  return Promise.resolve()
+    .then(() => {
       const email = `${config.mailbackMailbox}+${file.id}@mail.mailback.io`;
 
       return rp({
@@ -83,40 +158,29 @@ function archiveFile(file) {
 
       file.url = fileUrl;
       file.name = name;
+    });
+}
 
-      return rp({
-        uri: fileUrl,
-        resolveWithFullResponse: true,
-        encoding: null
-      });
-    })
-    .then(response => {
-      console.log(chalk.gray('File received'));
+function scrapeFile(file) {
+  console.log(`Archiving file ${file.id} (${file.category.title}/${file.title})`);
 
-      const data = response.body;
-      const shasum = crypto.createHash('sha1');
-
-      shasum.update(data);
-
-      // @todo make if-modified-since request
-      // and check lastModifiedAt && sha1sum and skip if file already is in saved manifest
-      // this would make the tool reentrant
-
-      file.lastModifiedAt = new Date(response.headers['last-modified']);
-      file.receivedAt = new Date();
-      file.sha1sum = shasum.digest('hex');
-
-      console.log(chalk.gray('Last modified:', file.lastModifiedAt.toLocaleString()));
-      console.log(chalk.gray('SHA-1 sum:', file.sha1sum));
-
-      return saveFile(data, file.name);
+  return Promise.resolve()
+    .then(() => {
+      return file.url
+        ? Promise.resolve()
+        : scrapeFileUrl(file);
     })
     .then(() => {
-      console.log(chalk.gray('File saved'));
+      return config.skipDownload || file.receivedAt
+        ? Promise.resolve()
+        : downloadFile(file);
+    })
+    .then(() => {
+      console.log(chalk.gray('File scraped'));
     })
     .catch(error => {
       console.error(chalk.red('Error archiving file:', error));
-    })
+    });
 }
 
 function writeManifest(fileList) {
@@ -136,61 +200,27 @@ function writeManifest(fileList) {
 console.log('Archiving EPA Geo Portal data to:', archiveDirectory);
 console.log(chalk.gray('Started at:', startedAt.toLocaleString()));
 
-Promise.resolve()
+ensureArchiveDirectoryExists()
   .then(() => {
-    console.log('Fetching index HTML');
-
-    return rp(config.indexUrl);
-  })
-  .then(html => {
-    console.log('Extracting file metadata');
-
-    const $ = cheerio.load(html);
-
-    const categoryList = $('.categories h3')
-      .map((idx, el) => {
-        const $el = $(el);
-
-        return {
-          id: $el.attr('class').replace(/\D/g, ''),
-          title: $el.text()
-        };
-      })
-      .get();
-
-    const categoryMap = {};
-
-    categoryList.forEach(category => {
-      categoryMap[category.id] = category;
-    });
-
-    return $('.SelectedFile')
-      .map((idx, el) => {
-        const $el = $(el);
-
-        const categoryId = $el.closest('p').attr('class').replace(/\D/g, '');
-
-        return {
-          id: $el.val(),
-          category: categoryMap[categoryId],
-          title: $el.parent().text()
-        };
-      })
-      .get();
+    try {
+      return require('./archive/manifest.json');
+    } catch (error) {
+      return fetchFileList();
+    }
   })
   .then(fileList => {
     console.log(chalk.gray(`Found ${fileList.length} file(s)`));
 
-    const numFilesToArchive = Math.min(fileList.length, config.maxDocs);
-    const archivableFileList = fileList.slice(config.startIdx, config.startIdx + numFilesToArchive);
+    const numFilesToScrape = Math.min(fileList.length, config.maxDocs);
+    const scrapeableFileList = fileList.slice(config.startIdx, config.startIdx + numFilesToScrape);
 
-    console.log(`Archiving ${archivableFileList.length} file(s)`);
+    console.log(`Archiving ${scrapeableFileList.length} file(s)`);
 
     return enqueue(
-      archivableFileList,
+      scrapeableFileList,
       (file) => {
-        return archiveFile(file)
-          .then(() => writeManifest(archivableFileList))
+        return scrapeFile(file)
+          .then(() => writeManifest(scrapeableFileList))
           .then(() => console.log(chalk.gray('Manifest written')));
       }
     );
